a59681f613a67c4374523f9c373d90da02680667
Shamim0209 <meshamim800@gmail.com>
2023-08-14 01:37:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Carousel, CarouselItem } from "./Carousel/Carousel";
+export default function App() {
+  const natureImages = [
+    "https://plus.unsplash.com/premium_photo-1661878193069-260470ba5da4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    "https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+    "https://images.unsplash.com/photo-1446776899648-aa78eefe8ed0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80",
+    "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1510852151262-05bfbfbe996d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80",
+  ];
+  return (
+    <div className="App">
+      <Carousel>
+        {natureImages.map((imageUrl, index) => (
+          <CarouselItem key={index} imageUrl={imageUrl} index={index+1} />
+        ))}
+      </Carousel>
+    </div>
+  );
+}
---
+++ src/Carousel/Carousel.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import "./Carousel.css";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+function CarouselItem(props) {
+  return (
+    <div className="carousel-item">
+      <img src={props.imageUrl} alt={`Nature Image ${props.index}`} />
+    </div>
+  );
+}
+
+const Carousel = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [paused, setPaused] = useState(false);
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = React.Children.count(children) - 1;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = 0;
+    }
+
+    setActiveIndex(newIndex);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!paused) {
+        updateIndex(activeIndex + 1);
+      }
+    }, 3000);
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [paused, activeIndex]);
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => updateIndex(activeIndex + 1),
+    onSwipedRight: () => updateIndex(activeIndex - 1),
+  });
+
+  return (
+    <div
+      {...handlers}
+      className="carousel"
+      onMouseEnter={() => setPaused(true)}
+      onMouseLeave={() => setPaused(false)}
+    >
+      <div
+        className="side-button left"
+        onClick={() => updateIndex(activeIndex - 1)}
+      >
+        <ArrowBackIosNewIcon />
+      </div>
+      <div
+        className="inner"
+        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+      >
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, { width: "100%" });
+        })}
+      </div>
+      <div
+        className="side-button right"
+        onClick={() => updateIndex(activeIndex + 1)}
+      >
+        <ArrowForwardIosIcon />
+      </div>
+      <div className="indicators">
+        {React.Children.map(children, (child, index) => {
+          return (
+            <button
+              className={`${index === activeIndex ? "active" : ""}`}
+              onClick={() => {
+                updateIndex(index);
+              }}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export { Carousel, CarouselItem };
